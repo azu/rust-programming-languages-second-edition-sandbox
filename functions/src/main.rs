a9bfe1cc0347c781fd1a28d2fee7aa283ad10053
azu <azuciao@gmail.com>
2019-01-06 21:47:27 +0900

    Add functions

---
+++ functions/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let y = {
+        let x = 3;
+        x + 1 // 式の場合は;をつけない
+    };
+    println!("y is {}", y);
+
+    another_function(5, 42);
+
+    let five = five();
+    println!("five is {}", five);
+    let six = plus_one(five);
+    println!("five + 1 is {}", six);
+
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("{}", x);
+    println!("{}", y);
+}
+
+fn five() -> i32 {
+    5 // これも式
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+    // x+1; だと() 空のタブルを返す事になっている
+}
