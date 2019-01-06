548406050cf751b7b5146a23de92167f544639dd
azu <azuciao@gmail.com>
2019-01-06 17:49:52 +0900

    feat(guessing_game): implement loop input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,26 +1,55 @@
+// randクレートの外部依存しているマーク
+extern crate rand;
+
+use std::cmp::Ordering;
 use std::io;
+
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
     // input
-    println!("Please input your guess.");
+    println!("Please input your guess number.");
 
-    // Rustはデフォルトでimmutableなのでmutでmutableにしている
-    let mut guess = String::new();
-    // &は参照を表す記号 = コピーしない
-    let result = io::stdin().read_line(&mut guess);
-    // InputのResultを返して、Resultは列挙型
-    // expectはErrの時にクラッシュさせる
-    /* expectしてない場合はwarningがでる = エラー処理しないといけない
-      --> src/main.rs:11:5
-   |
-11 |     io::stdin().read_line(&mut guess);
-   |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-   |
-   = note: #[warn(unused_must_use)] on by default
-   = note: this `Result` may be an `Err` variant, which should be handled
-   */
-    result.expect("Failed to read line");
-    // {} は プレースホルダー
-    println!("You guessed: {}", guess);
+    // 乱数を取得 1 - 100
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        // Rustはデフォルトでimmutableなのでmutでmutableにしている
+        let mut guess = String::new();
+        // &は参照を表す記号 = コピーしない
+        let result = io::stdin().read_line(&mut guess);
+        // InputのResultを返して、Resultは列挙型
+        // expectはErrの時にクラッシュさせる
+        /* expectしてない場合はwarningがでる = エラー処理しないといけない
+          --> src/main.rs:11:5
+       |
+    11 |     io::stdin().read_line(&mut guess);
+       |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+       |
+       = note: #[warn(unused_must_use)] on by default
+       = note: this `Result` may be an `Err` variant, which should be handled
+       */
+        result.expect("Failed to read line");
+
+        // guessを数値に変換
+        // シャドーイングによって変数名を再利用できる
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        // {} は プレースホルダー
+        println!("You guessed: {}", guess);
+
+        // マッチングする
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),       //小さすぎ！
+            Ordering::Greater => println!("Too big!"),      //大きすぎ！
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }        //やったね！
+        }
+    }
 }
