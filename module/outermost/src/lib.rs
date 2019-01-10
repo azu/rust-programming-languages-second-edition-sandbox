894b1308a72c802758c83e09581b68abe4bcdee8
azu <azuciao@gmail.com>
2019-01-10 17:55:56 +0900

    Add  outermost

---
+++ module/outermost/src/lib.rs
@@ -0,0 +1,18 @@
+mod outermost {
+    pub fn middle_function() {}
+
+    fn middle_secret_function() {}
+
+    mod inside {
+        pub fn inner_function() {}
+
+        fn secret_function() {}
+    }
+}
+
+fn try_me() {
+    outermost::middle_function();
+    outermost::middle_secret_function();
+    outermost::inside::inner_function();
+    outermost::inside::secret_function();
+}
