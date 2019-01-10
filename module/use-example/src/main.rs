0ccf6e877f766efb6d33c285aa755f8c364e1601
azu <azuciao@gmail.com>
2019-01-10 18:02:32 +0900

    Add use-example

---
+++ module/use-example/src/main.rs
@@ -0,0 +1,14 @@
+pub mod a {
+    pub mod series {
+        pub mod of {
+            pub fn nested_modules() {}
+        }
+    }
+}
+
+use a::series::of;
+
+fn main() {
+    of::nested_modules();
+
+}
